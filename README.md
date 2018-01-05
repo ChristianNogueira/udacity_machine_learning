b690dfcd16b3693aaeb9cf4cd03f7bea50a53660
Christian S Nogueira <chrnogueira@gmail.com>
2018-01-04 23:32:35 -0200

    primeira entrega do projeto

--- README.md
+++ README.md
@@ -2,3 +2,22 @@
 
 Repositório do projeto final.
 [Udacity](https://www.udacity.com)
+
+*final_project/final_project.ipynb*
+Projeto detalhado
+
+*final_project/convert_python2_json.ipynb*
+Utilizado para converter o arquivo pickle fornecido para json
+
+*final_project/convert_json_pickle.ipynb*
+Utilizado para converter o json em pickle para python 3
+
+*my_classifier.pkl*
+Objeto classifier (clf) final, salvo em python 3
+
+*my_dataset.pkl*
+Dicionário final com os dados tratados, salvo em python 3
+
+*my_feature_list.pkl*
+Lista com as variáveis utilizadas, salvo em python 3
+
--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import sys
 import pickle
@@ -7,17 +7,27 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 
-### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list = ['poi', 'exercised_stock_options', 'total_stock_value', 'bonus', 'salary']
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
 ### Task 2: Remove outliers
+items_to_drop = ['TOTAL','THE TRAVEL AGENCY IN THE PARK', 'LOCKHART EUGENE E']
+for outlier in items_to_drop:
+    data_dict.pop(outlier, 0)
+    
 ### Task 3: Create new feature(s)
+if_nan_make_zero = lambda x : 0 if x == 'NaN' else x 
+
+for poi in data_dict:
+    if data_dict[poi]['to_messages'] == 'NaN':
+        data_dict[poi]['msg_to_poi_prec'] = 0
+    else:
+        data_dict[poi]['msg_to_poi_prec'] = if_nan_make_zero(data_dict[poi]['from_this_person_to_poi']) / \
+                                if_nan_make_zero(data_dict[poi]['to_messages'])
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
@@ -25,31 +35,10 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
-# Provided to give you a starting point. Try a variety of classifiers.
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-
-# Example starting point. Try investigating other evaluation techniques!
-from sklearn.cross_validation import train_test_split
-features_train, features_test, labels_train, labels_test = \
-    train_test_split(features, labels, test_size=0.3, random_state=42)
-
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
+clf = make_pipeline(StandardScaler(), GaussianNB())
 
 dump_classifier_and_data(clf, my_dataset, features_list)
